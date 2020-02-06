63cb05249f576bf641b38de3260f827cd213708b
graham-dds <graham.beckley-ctr@friends.dds.mil>
2020-02-06 14:45:05 -0500

    Add Reports domain method to get portfolio spend

--- atst/domain/reports.py
+++ atst/domain/reports.py
@@ -1,5 +1,11 @@
 from flask import current_app
 from itertools import groupby
+from atst.domain.csp.cloud.models import (
+    ReportingCSPPayload,
+    CostManagementQueryCSPResult,
+)
+from atst.domain.csp.reports import prepare_azure_reporting_data
+import pendulum
 
 
 class Reports:
@@ -42,3 +48,21 @@ class Reports:
                 }
             )
         return output
+
+    @classmethod
+    def get_portfolio_spending(cls, portfolio):
+        # TODO: Extend this function to make from_date and to_date configurable
+        from_date = pendulum.now().subtract(years=1).add(days=1).format("YYYY-MM-DD")
+        to_date = pendulum.now().format("YYYY-MM-DD")
+        rows = []
+
+        if portfolio.csp_data:
+            payload = ReportingCSPPayload(
+                from_date=from_date, to_date=to_date, **portfolio.csp_data
+            )
+            response: CostManagementQueryCSPResult = current_app.csp.cloud.get_reporting_data(
+                payload
+            )
+            rows = response.properties.rows
+
+        return prepare_azure_reporting_data(rows)
