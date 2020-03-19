5ca95c41f2b8dbf7e25381ddb428b9b5e37c2202
Jesse Bracho <jessebracho.dds@gmail.com>
2020-03-19 15:17:24 -0400

    Create HybridCSP class
    Co-authored-by: graham-dds <graham.beckley-ctr@friends.dds.mil>

--- atat/domain/csp/__init__.py
+++ atat/domain/csp/__init__.py
@@ -1,4 +1,6 @@
-from .cloud import MockCloudProvider
+import os
+
+from .cloud import MockCloudProvider, HybridCloudProvider, AzureCloudProvider
 from .files import AzureFileService, MockFileService
 from .reports import MockReportingProvider
 
@@ -22,10 +24,37 @@ class AzureCSP:
         self.reports = MockReportingProvider()
 
 
+class HybridCSP:
+    def __init__(self, app, test_mode=False):
+        app.config.update(
+            {
+                "AZURE_CLIENT_ID": os.environ.get("AZURE_CLIENT_ID"),
+                "AZURE_SECRET_KEY": os.environ.get("AZURE_SECRET_KEY"),
+                "AZURE_TENANT_ID": os.environ.get("AZURE_TENANT_ID"),
+                "AZURE_VAULT_URL": os.environ.get("AZURE_VAULT_URL"),
+                "AZURE_POWERSHELL_CLIENT_ID": os.environ.get(
+                    "AZURE_POWERSHELL_CLIENT_ID"
+                ),
+            }
+        )
+        azure = AzureCloudProvider(app.config)
+        mock = MockCloudProvider(
+            app.config,
+            with_delay=(not test_mode),
+            with_failure=(not test_mode),
+            with_authorization=(not test_mode),
+        )
+        self.cloud = HybridCloudProvider(azure, mock)
+        self.files = MockFileService(app)
+        self.reports = MockReportingProvider()
+
+
 def make_csp_provider(app, csp=None):
     if csp == "azure":
         app.csp = AzureCSP(app)
     elif csp == "mock-test":
         app.csp = MockCSP(app, test_mode=True)
+    elif csp == "hybrid":
+        app.csp = HybridCSP(app, test_mode=True)
     else:
         app.csp = MockCSP(app)
