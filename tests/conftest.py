37544e56d39fe23af5bbdcbd2556043c98289e32
graham-dds <graham.beckley-ctr@friends.dds.mil>
2020-03-31 15:22:15 -0400

    Add pytset option to repeat test suite n times

--- tests/conftest.py
+++ tests/conftest.py
@@ -27,6 +27,18 @@ dictConfig({"version": 1, "handlers": {"wsgi": {"class": "logging.NullHandler"}}
 
 def pytest_addoption(parser):
     parser.addoption("--hybrid", action="store_true", default=False)
+    parser.addoption(
+        "--repeat", action="store", help="Number of times to repeat each test"
+    )
+
+
+def pytest_generate_tests(metafunc):
+    if metafunc.config.option.repeat is not None:
+        count = int(metafunc.config.option.repeat)
+
+        # Append fixture that accepts how many times each test should be repeated
+        metafunc.fixturenames.append("tmp_ct")
+        metafunc.parametrize("tmp_ct", range(count))
 
 
 def pytest_collection_modifyitems(config, items):
