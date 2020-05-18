ab8fbc675ecb5fa92791bf76e315e6eff4da4924
mikepw <michaelpapetti@Michaels-MBP.fios-router.home>
2020-05-18 14:59:55 -0400

    parameterizes

--- script/tf_plan.sh
+++ script/tf_plan.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 
-if [ -z $DEPLOY_TAG] || [ -z $TF_DIR] || [ -z $VAULT_SECRET ] || [ -z $VAULT_SECRET ] || [ -z $VAULT_CLIENT_ID ] ||  [ -z $VAULT_TENANT ] ||  [ -z $SUBSCRIPTION_ID ]
+if [ -z $DEPLOY_TAG] || [ -z $TF_DIR] || [ -z $VAULT_SECRET ] || [ -z $VAULT_URL ] || [ -z $VAULT_CLIENT_ID ] ||  [ -z $VAULT_TENANT ] ||  [ -z $SUBSCRIPTION_ID ]
 then
 echo "Set DEPLOY_TAG, TF_DIR, VAULT_SECRET, VAULT_CLIENT_ID, VAULT_TENANT, SUBSCRIPTION_ID"
 exit 1;
 fi
 
-poetry run ansible-playbook ../ansible/site.yml --extra-vars "provision_pwdev=true deploy_tag=$DEPLOY_TAG tf_dir='$TF_DIR' vault_url='https://ops-pwdev-keyvault.vault.azure.net/' vault_secret='$VAULT_SECRET' vault_client_id='$VAULT_CLIENT_ID' vault_tenant='$VAULT_TENANT' vault_subscription_id='$SUBSCRIPTION_ID'"
+poetry run ansible-playbook ../ansible/site.yml --extra-vars "provision_pwdev=true deploy_tag=$DEPLOY_TAG tf_dir='$TF_DIR' vault_url='$VAULT_URL' vault_secret='$VAULT_SECRET' vault_client_id='$VAULT_CLIENT_ID' vault_tenant='$VAULT_TENANT' vault_subscription_id='$SUBSCRIPTION_ID'"
 
 terraform show $TF_DIR/plan.tfplan
 
