4b8882b2e292bbee277d6181600a178a151c5eae
mikepw <michaelpapetti@Michaels-MBP.fios-router.home>
2020-05-20 14:25:21 -0400

    adds azure bastion, bastion subnet, mgmt subnet, bastion k8s single node cluster

--- terraform/providers/pwdev/bastion.tf
+++ terraform/providers/pwdev/bastion.tf
@@ -14,6 +14,7 @@ module "bastion" {
   bastion_subnet_vpc_name = module.vpc.vpc_name
   mgmt_subnet_cidr        = "10.1.250.0/24"
   bastion_subnet_cidr     = "10.1.4.0/24"
+<<<<<<< HEAD
   bastion_aks_sp_secret   = var.bastion_aks_sp_secret
   bastion_aks_sp_id       = var.bastion_aks_sp_id
   environment             = var.environment
@@ -23,6 +24,13 @@ module "bastion" {
   bastion_ssh_pub_key_path   = var.bastion_ssh_pub_key_path
   log_analytics_workspace_id = var.log_analytics_workspace_id
 
+=======
+  bastion_aks_sp_secret = var.bastion_aks_sp_secret
+  bastion_aks_sp_id = var.bastion_aks_sp_id
+  environment = var.environment
+  owner = var.owner
+  name = var.name
+>>>>>>> adds azure bastion, bastion subnet, mgmt subnet, bastion k8s single node cluster
 
 
 }
