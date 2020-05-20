504009d10cff366b0f7a818edf6eb8aa983c2a0e
mikepw <michaelpapetti@Michaels-MBP.fios-router.home>
2020-05-20 14:52:09 -0400

    updates terraform config to remove deprecated warnings

--- terraform/modules/k8s/main.tf
+++ terraform/modules/k8s/main.tf
@@ -12,7 +12,7 @@ resource "azurerm_kubernetes_cluster" "k8s" {
   location             = azurerm_resource_group.k8s.location
   resource_group_name  = azurerm_resource_group.k8s.name
   dns_prefix           = var.k8s_dns_prefix
-  private_link_enabled = var.private_link_enabled
+  private_cluster_enabled = var.private_link_enabled
 
   service_principal {
     client_id     = var.client_id
