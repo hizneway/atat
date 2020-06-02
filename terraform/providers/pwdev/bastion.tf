1fdbbeaf070ef16157a862b197c130032df10343
mikepw <michaelpapetti@Michaels-MBP.fios-router.home>
2020-06-02 08:33:29 -0400

    fixes merge conflicts

--- terraform/providers/pwdev/bastion.tf
+++ terraform/providers/pwdev/bastion.tf
@@ -14,31 +14,14 @@ module "bastion" {
   bastion_subnet_vpc_name = module.vpc.vpc_name
   mgmt_subnet_cidr        = "10.1.250.0/24"
   bastion_subnet_cidr     = "10.1.4.0/24"
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> formats terraform, updates provider
   bastion_aks_sp_secret   = var.bastion_aks_sp_secret
   bastion_aks_sp_id       = var.bastion_aks_sp_id
   environment             = var.environment
   owner                   = var.owner
   name                    = var.name
-<<<<<<< HEAD
-
   bastion_ssh_pub_key_path   = var.bastion_ssh_pub_key_path
   log_analytics_workspace_id = var.log_analytics_workspace_id
 
-=======
-  bastion_aks_sp_secret = var.bastion_aks_sp_secret
-  bastion_aks_sp_id = var.bastion_aks_sp_id
-  environment = var.environment
-  owner = var.owner
-  name = var.name
->>>>>>> adds azure bastion, bastion subnet, mgmt subnet, bastion k8s single node cluster
-=======
->>>>>>> formats terraform, updates provider
-
-  bastion_ssh_pub_key_path = var.bastion_ssh_pub_key_path
 
 
 }
