0a5f2feceeea866caeef8e006e430be3bd83377b
CJ Green <cj+atat@promptworks.com>
2020-07-29 16:34:37 -0400

    Change resourceScope to directoryScopeId

--- atat/domain/csp/cloud/azure_cloud_provider.py
+++ atat/domain/csp/cloud/azure_cloud_provider.py
@@ -1154,7 +1154,7 @@ class AzureCloudProvider(CloudProviderInterface):
         request_body = {
             "principalId": payload.principal_id,
             "roleDefinitionId": payload.admin_role_def_id,
-            "resourceScope": "/",
+            "directoryScopeId": "/",
         }
 
         auth_header = {
@@ -1261,7 +1261,7 @@ class AzureCloudProvider(CloudProviderInterface):
         request_body = {
             "roleDefinitionId": billing_admin_role_id,
             "principalId": user_id,
-            "resourceScope": "/",
+            "directoryScopeId": "/",
         }
 
         auth_header = {
