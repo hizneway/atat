54d3358b3963eabb3065db9733de3fb8281af8f1
Jesse Bracho <jessebracho.dds@gmail.com>
2020-07-29 10:24:21 -0400

    `resource` and `tenant_id` were undefined

--- atat/domain/csp/cloud/azure_cloud_provider.py
+++ atat/domain/csp/cloud/azure_cloud_provider.py
@@ -188,19 +188,18 @@ class AzureCloudProvider(CloudProviderInterface):
     @log_and_raise_exceptions
     def _get_keyvault_token(self):
         url = f"{self.sdk.cloud.endpoints.active_directory}/{self.root_tenant_id}/oauth2/token"
+        resource = f"https://{self.sdk.cloud.suffixes.keyvault_dns[1:]}"
         payload = {
             "grant_type": "client_credentials",
             "client_id": self.client_id,
             "client_secret": self.secret_key,
-            "resource": f"https://{self.sdk.cloud.suffixes.keyvault_dns[1:]}",
+            "resource": resource,
         }
         token_response = self.sdk.requests.get(url, data=payload, timeout=30)
         token_response.raise_for_status()
         token = token_response.json().get("access_token")
         if token is None:
-            message = (
-                f"Failed to get token for resource '{resource}' in tenant '{tenant_id}'"
-            )
+            message = f"Failed to get token for resource '{resource}' in tenant '{self.root_tenant_id}'"
             app.logger.error(message, exc_info=1)
             raise AuthenticationException(message)
         else:
