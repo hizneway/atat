8f9f20f17bf98b657e21c15616de9ec8ce44cd6a
tomdds <tom.chandler-ctr@friends.dds.mil>
2020-08-11 10:51:39 -0400

    Remove unused terraform providers
    In addition to removing prod-eastus because it contained some semi-sensitive data (ip addresses) this also cleans up other unused terraform providers.

--- terraform/providers/pwdev/logs.tf
+++
@@ -1,9 +0,0 @@
-module "logs" {
-  source            = "../../modules/log_analytics"
-  owner             = var.owner
-  environment       = var.environment
-  region            = var.region
-  name              = var.name
-  retention_in_days = 365
-}
-
