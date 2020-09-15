74e5e9eb3172dbf54ae4691f4826c1bf87b1f4a0
tomdds <tom.chandler-ctr@friends.dds.mil>
2020-09-15 13:53:45 -0400

    Use test class and fixture for saml attribute processing tests

--- tests/routes/test_saml_helpers.py
+++ tests/routes/test_saml_helpers.py
@@ -216,97 +216,82 @@ class TestGetIdPConfig:
         assert mock_parser.parse_remote.call_count == 1
 
 
-def test_get_user_from_saml_attributes():
-    expected_dod_id = "1234567890"
-    saml_attributes = {
-        EIFSAttributes.GIVEN_NAME: "",
-        EIFSAttributes.LAST_NAME: "",
-        EIFSAttributes.EMAIL: "",
-        EIFSAttributes.SAM_ACCOUNT_NAME: f"{expected_dod_id}.MIL",
-        EIFSAttributes.US_CITIZEN: "Y",
-        EIFSAttributes.AGENCY_CODE: "F",
-        EIFSAttributes.MOBILE: "",
-    }
-    user = get_user_from_saml_attributes(saml_attributes)
-    assert user.dod_id == expected_dod_id
-    assert user.designation == "military"
-    assert user.citizenship == "United States"
-    assert user.service_branch == "air_force"
+class TestSamlAttributes:
+    @pytest.fixture(scope="function")
+    def mock_attributes(self):
+        return {
+            EIFSAttributes.GIVEN_NAME: "",
+            EIFSAttributes.LAST_NAME: "",
+            EIFSAttributes.EMAIL: "",
+            EIFSAttributes.SAM_ACCOUNT_NAME: "",
+            EIFSAttributes.US_CITIZEN: "",
+            EIFSAttributes.AGENCY_CODE: "",
+            EIFSAttributes.MOBILE: "",
+        }
 
+    def test_get_user_from_saml_attributes(self, mock_attributes):
+        expected_dod_id = "1234567890"
+        saml_attributes = {
+            **mock_attributes,
+            **{
+                EIFSAttributes.SAM_ACCOUNT_NAME: f"{expected_dod_id}.MIL",
+                EIFSAttributes.US_CITIZEN: "Y",
+                EIFSAttributes.AGENCY_CODE: "F",
+            },
+        }
+        user = get_user_from_saml_attributes(saml_attributes)
+        assert user.dod_id == expected_dod_id
+        assert user.designation == "military"
+        assert user.citizenship == "United States"
+        assert user.service_branch == "air_force"
 
-def test_get_user_from_saml_attributes_missing_dod_id():
-    saml_attributes = {
-        EIFSAttributes.GIVEN_NAME: "",
-        EIFSAttributes.LAST_NAME: "",
-        EIFSAttributes.EMAIL: "",
-        EIFSAttributes.US_CITIZEN: "",
-        EIFSAttributes.AGENCY_CODE: "",
-        EIFSAttributes.MOBILE: "",
-    }
-    with pytest.raises(Exception):
-        get_user_from_saml_attributes(saml_attributes)
+    def test_get_user_from_saml_attributes_missing_dod_id(self, mock_attributes):
+        with pytest.raises(Exception):
+            get_user_from_saml_attributes(mock_attributes)
 
+    def test_get_user_from_saml_invalid_sam_format(self, mock_attributes):
+        saml_attributes = {
+            **mock_attributes,
+            **{EIFSAttributes.SAM_ACCOUNT_NAME: "sam account name format changed",},
+        }
+        with pytest.raises(Exception):
+            get_user_from_saml_attributes(saml_attributes)
 
-def test_get_user_from_saml_invalid_sam_format():
-    saml_attributes = {
-        EIFSAttributes.GIVEN_NAME: "",
-        EIFSAttributes.LAST_NAME: "",
-        EIFSAttributes.SAM_ACCOUNT_NAME: "sam account name format changed",
-        EIFSAttributes.EMAIL: "",
-        EIFSAttributes.US_CITIZEN: "",
-        EIFSAttributes.AGENCY_CODE: "",
-        EIFSAttributes.MOBILE: "",
-    }
-    with pytest.raises(Exception):
-        get_user_from_saml_attributes(saml_attributes)
+    def test_get_user_from_saml_existing_user(self, mock_attributes):
+        expected_dod_id = "1234567890"
+        saml_attributes = {
+            **mock_attributes,
+            **{EIFSAttributes.SAM_ACCOUNT_NAME: f"{expected_dod_id}.MIL",},
+        }
+        expected_user = UserFactory.create(dod_id=expected_dod_id)
 
+        assert expected_user == get_user_from_saml_attributes(saml_attributes)
 
-def test_get_user_from_saml_existing_user():
-    expected_dod_id = "1234567890"
-    saml_attributes = {
-        EIFSAttributes.GIVEN_NAME: "",
-        EIFSAttributes.LAST_NAME: "",
-        EIFSAttributes.EMAIL: "",
-        EIFSAttributes.SAM_ACCOUNT_NAME: f"{expected_dod_id}.MIL",
-        EIFSAttributes.US_CITIZEN: "",
-        EIFSAttributes.AGENCY_CODE: "",
-        EIFSAttributes.MOBILE: "",
-    }
-    expected_user = UserFactory.create(dod_id=expected_dod_id)
+    def test_get_user_from_saml_telephone_over_mobile(self, mock_attributes):
+        expected_dod_id = "1234567890"
+        saml_attributes = {
+            **mock_attributes,
+            **{
+                EIFSAttributes.SAM_ACCOUNT_NAME: f"{expected_dod_id}.MIL",
+                EIFSAttributes.MOBILE: "1234",
+                EIFSAttributes.TELEPHONE: "5678",
+            },
+        }
 
-    assert expected_user == get_user_from_saml_attributes(saml_attributes)
+        user = get_user_from_saml_attributes(saml_attributes)
 
+        assert user.phone_number == "5678"
 
-def test_get_user_from_saml_telephone_over_mobile():
-    expected_dod_id = "1234567890"
-    saml_attributes = {
-        EIFSAttributes.GIVEN_NAME: "",
-        EIFSAttributes.LAST_NAME: "",
-        EIFSAttributes.EMAIL: "",
-        EIFSAttributes.SAM_ACCOUNT_NAME: f"{expected_dod_id}.MIL",
-        EIFSAttributes.US_CITIZEN: "",
-        EIFSAttributes.AGENCY_CODE: "",
-        EIFSAttributes.MOBILE: "1234",
-        EIFSAttributes.TELEPHONE: "5678",
-    }
+    def test_get_user_from_saml_mobile_fallback(self, mock_attributes):
+        expected_dod_id = "1234567890"
+        saml_attributes = {
+            **mock_attributes,
+            **{
+                EIFSAttributes.SAM_ACCOUNT_NAME: f"{expected_dod_id}.MIL",
+                EIFSAttributes.MOBILE: "1234",
+            },
+        }
 
-    user = get_user_from_saml_attributes(saml_attributes)
+        user = get_user_from_saml_attributes(saml_attributes)
 
-    assert user.phone_number == "5678"
-
-
-def test_get_user_from_saml_mobile_fallback():
-    expected_dod_id = "1234567890"
-    saml_attributes = {
-        EIFSAttributes.GIVEN_NAME: "",
-        EIFSAttributes.LAST_NAME: "",
-        EIFSAttributes.EMAIL: "",
-        EIFSAttributes.SAM_ACCOUNT_NAME: f"{expected_dod_id}.MIL",
-        EIFSAttributes.US_CITIZEN: "",
-        EIFSAttributes.AGENCY_CODE: "",
-        EIFSAttributes.MOBILE: "1234",
-    }
-
-    user = get_user_from_saml_attributes(saml_attributes)
-
-    assert user.phone_number == "1234"
+        assert user.phone_number == "1234"
