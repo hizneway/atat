54ad5691daeef02e87305c70cb3b4baa2b36c7e8
mikepw <mike@opsulent.com>
2020-10-01 10:12:38 -0400

    fixes terraform state push to boostrap env

--- terraform/providers/bootstrap/buckets.tf
+++ terraform/providers/bootstrap/buckets.tf
@@ -11,7 +11,7 @@ module "tf_state" {
   region                 = var.region
   policy                 = "Deny"
   subnet_ids             = []
-  whitelist              = merge(var.storage_admin_whitelist, { "operator" : chomp(data.http.myip.body) })
+  whitelist              = merge(var.storage_admin_whitelist, { "operator" : chomp(data.http.myip.body) },{ "opsnet": var.virtual_network })
   account_kind           = "Storage"
   storage_container_name = var.storage_container_name
 }
