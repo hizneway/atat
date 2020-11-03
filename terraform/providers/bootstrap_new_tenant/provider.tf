dafa687df5bb39f0b9399ad1e57a68a0abfe87a1
Jesse Bracho <jessebracho.dds@gmail.com>
2020-11-03 11:52:49 -0500

    Copy bootstrap `provider.tf` over to bootstrap_new_tenant folder

---
+++ terraform/providers/bootstrap_new_tenant/provider.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "= 1.0.0"
+    }
+
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "= 2.34.0"
+    }
+  }
+}
+
+resource "random_pet" "unique_id" {
+  length    = 1
+  separator = ""
+}
+
+locals {
+  environment = length(var.environment) > 0 ? var.environment : random_pet.unique_id.id
+  location = "East US"
+}
+
+resource "azurerm_resource_group" "bootstrap_resource_group" {
+  name = "bootstrap_resource_group_${locals.environment}"
+  location = locals.location 
+}
