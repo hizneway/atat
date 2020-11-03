b4d13d940b4bec7dfa59e8f710ba26c73222aefa
Jesse Bracho <jessebracho.dds@gmail.com>
2020-11-03 16:02:53 -0500

    Export deployment subnet

--- terraform/providers/bootstrap_new_tenant/variables.tf
+++ terraform/providers/bootstrap_new_tenant/variables.tf
@@ -1,11 +1,11 @@
+variable "operations_location" {
+  type        = string
+  default     = "East US"
+  description = "Azure region in which operations resources are provisioned."
+}
+
 variable "operations_namespace" {
   type        = string
   default     = "dev"
   description = "Namespace of provisioned operations resources."
-}
-
-variable "location" {
-  type        = string
-  default     = "East US"
-  description = "Azure region in which operations resources are provisioned."
 }
