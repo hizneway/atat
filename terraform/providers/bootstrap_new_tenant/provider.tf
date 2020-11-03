9edb54c28b177d56e0bf103d7bb7078c2679e2a8
Jesse Bracho <jessebracho.dds@gmail.com>
2020-11-03 13:54:56 -0500

    Bandaid issue with `azurerm` provider configuration

--- terraform/providers/bootstrap_new_tenant/provider.tf
+++ terraform/providers/bootstrap_new_tenant/provider.tf
@@ -12,12 +12,21 @@ terraform {
   }
 }
 
+# Specifying a `provider` this way is deprecated. It is necessary in this 
+# situation due to an issue with the `azurerm` provider. 
+#
+# https://www.terraform.io/docs/configuration/providers.html#version-an-older-way-to-manage-provider-versions
+# https://github.com/terraform-providers/terraform-provider-azurerm/issues/7359
+provider "azurerm" {
+  features {}
+}
+
 locals {
   environment = "jesse"
   location    = "East US"
 }
 
 resource "azurerm_resource_group" "bootstrap_resource_group" {
-  name     = "bootstrap_resource_group_${locals.environment}"
-  location = locals.location
+  name     = "bootstrap_resource_group_${local.environment}"
+  location = local.location
 }
