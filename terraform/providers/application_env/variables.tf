e964a18482366e287e7aa4021cc518713c9e2061
Jesse Bracho <jessebracho.dds@gmail.com>
2020-11-06 12:07:06 -0500

    Restore the pass-through variables

--- terraform/providers/application_env/variables.tf
+++ terraform/providers/application_env/variables.tf
@@ -182,26 +182,29 @@ variable "OPS_SP_URL" {
   default = "asdfasd"
 }
 
-# variable "mailgun_smtp_password" {}
+# Pass-through variables.
+# These are variables which need to appear in the terraform output to configure
+# kubernetes but DO NOT configure the terraform in any appreciable way.
 
-# variable "azure_subscription_id" {}
-# variable "azure_hybrid_tenant_id" {}
-# variable "azure_hybrid_user_object_id" {}
-# variable "azure_hybrid_tenant_admin_password" {}
-# variable "AZURE-BILLING-ACCOUNT-NAME" {}
-# variable "AZURE-INVOICE-SECTION-ID" {}
-# variable "SAML-IDP-CERT" {}
-# variable "AZURE-BILLING-PROFILE-ID" {}
-# variable "dhparam4096" {}
-# variable "AZURE_SUBSCRIPTION_CREATION_CLIENT_ID" {}
-# variable "AZURE_SUBSCRIPTION_CREATION_SECRET" {}
-# variable "AZURE_POWERSHELL_CLIENT_ID" {}
-# variable "AZURE_ROOT_MGMT_GROUP_ID" {}
-# variable "AZURE_TENANT_ADMIN_USERNAME" {}
-# variable "AZURE_TENANT_ID" {}
-# variable "AZURE_USER_OBJECT_ID" {}
-# variable "CSP" {}
-# variable "AZURE_HYBRID_REPORTING_CLIENT_ID" {}
-# variable "AZURE_HYBRID_REPORTING_SECRET" {}
-# variable "circle_ci_api_key" {}
-# variable "deployment_subnet_id" {}
+variable "mailgun_smtp_password" {}
+variable "azure_subscription_id" {}
+variable "azure_hybrid_tenant_id" {}
+variable "azure_hybrid_user_object_id" {}
+variable "azure_hybrid_tenant_admin_password" {}
+variable "AZURE-BILLING-ACCOUNT-NAME" {}
+variable "AZURE-INVOICE-SECTION-ID" {}
+variable "SAML-IDP-CERT" {}
+variable "AZURE-BILLING-PROFILE-ID" {}
+variable "dhparam4096" {}
+variable "AZURE_SUBSCRIPTION_CREATION_CLIENT_ID" {}
+variable "AZURE_SUBSCRIPTION_CREATION_SECRET" {}
+variable "AZURE_POWERSHELL_CLIENT_ID" {}
+variable "AZURE_ROOT_MGMT_GROUP_ID" {}
+variable "AZURE_TENANT_ADMIN_USERNAME" {}
+variable "AZURE_TENANT_ID" {}
+variable "AZURE_USER_OBJECT_ID" {}
+variable "CSP" {}
+variable "AZURE_HYBRID_REPORTING_CLIENT_ID" {}
+variable "AZURE_HYBRID_REPORTING_SECRET" {}
+variable "circle_ci_api_key" {}
+variable "deployment_subnet_id" {}
