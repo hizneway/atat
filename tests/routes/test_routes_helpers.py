c06d07d83054b116882e16f826b4eea2431dc886
Christopher Green <cj+atat@promptworks.com>
2020-11-16 12:50:26 -0500

    Add tests for routes_helpers

--- tests/routes/test_routes_helpers.py
+++ tests/routes/test_routes_helpers.py
@@ -1,9 +1,23 @@
 from atat.routes.routes_helpers import match_url_pattern
 
 
-def test_valid_url():
-    assert match_url_pattern("/home")
+def test_match_url_patter():
+    # checking good urls
+    assert not any(
+        x is None
+        for x in (
+            match_url_pattern("/home"),
+            match_url_pattern("/login", "POST"),
+            match_url_pattern("/login"),
+        )
+    )
+    # checking bad urls
+    assert all(
+        x is None
+        for x in (
+            match_url_pattern("http://not.a.real.url.gov/#"),
+            match_url_pattern("http://not.a.real.url.gov/#", "POST"),
+            match_url_pattern("/home", "POST"),
+        )
+    )
 
-
-def test_invalid_url():
-    assert not match_url_pattern("http://not.a.real.url.gov/#")
