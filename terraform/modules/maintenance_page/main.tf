696e578e35d5565afe29e25e7a9a73b41f8e3519
mikepw <mike@opsulent.com>
2020-11-18 10:01:19 -0500

    adds maintenance page module

---
+++ terraform/modules/maintenance_page/main.tf
@@ -0,0 +1,55 @@
+
+
+resource "azurerm_network_profile" "maintenance_page" {
+  name                = "maintenance_page"
+  location            = var.location
+  resource_group_name = var.resource_group
+
+  container_network_interface {
+    name = "maintenancepagenic"
+
+    ip_configuration {
+      name      = "maintenancepageip"
+      subnet_id = var.subnet_id
+    }
+  }
+}
+
+
+
+resource "azurerm_container_group" "nginx_maintenance_page" {
+  name                = "${var.name}-maintenance-page-${var.environment}"
+  location            = var.location
+  resource_group_name = var.resource_group
+  ip_address_type     = "private"
+  network_profile_id  = azurerm_network_profile.maintenance_page.id
+  os_type             = "Linux"
+
+  container {
+    name     = "maintenance_page"
+    image    = "${var.ops_container_registry}/${var.nginx_container_image}"
+    cpu      = "1"
+    memory   = "2"
+    commands = ["nginx", "-g", "daemon off;"]
+
+    ports {
+      port     = 443
+      protocol = "TCP"
+    }
+  }
+
+  image_registry_credential {
+
+    username = var.registry_username
+    password = var.registry_password
+    server   = var.container_registry
+
+  }
+
+
+
+
+  tags = {
+    environment = var.environment
+  }
+}
---
+++ terraform/modules/maintenance_page/variables.tf
@@ -0,0 +1,14 @@
+variable "name" {}
+variable "enviroment" {}
+variable "resource_group" {}
+
+variable "location" {}
+
+variable "ops_container_registry" {}
+
+variable "nginx_container_image" {}
+
+
+variable "registry_username" {}
+variable "registry_password" {}
+variable "container_registry" {}
