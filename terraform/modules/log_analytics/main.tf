d38f7676cfbda7b46a512ab47cb960f7c8e796cd
Jeff Deville <jeff@promptworks.com>
2020-11-17 22:27:46 -0500

    Clean up log analytics workspace name

--- terraform/modules/log_analytics/main.tf
+++ terraform/modules/log_analytics/main.tf
@@ -1,10 +1,10 @@
 resource "azurerm_resource_group" "log_workspace" {
-  name     = "${var.name}-log-workspace-${var.environment}"
+  name     = "${var.name}-log-workspace"
   location = var.region
 }
 
 resource "azurerm_log_analytics_workspace" "log_workspace" {
-  name                = "${var.name}-log-workspace-${var.environment}"
+  name                = "${var.name}-log-workspace"
   location            = azurerm_resource_group.log_workspace.location
   resource_group_name = azurerm_resource_group.log_workspace.name
   sku                 = "Premium"
