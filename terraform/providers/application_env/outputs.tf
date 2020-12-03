c31e00ad6f363361418fea4d0a1fc05e2e8e2707
Jesse Bracho <jesse@promptwotks.com>
2020-12-03 14:11:17 -0500

    Attach to ops container registry context

--- terraform/providers/application_env/outputs.tf
+++ terraform/providers/application_env/outputs.tf
@@ -128,9 +128,9 @@ output "vnet_id" {
   value = module.vpc.id
 }
 
-# output "ops_container_registry_name" {
-#   value = data.terraform_remote_state.previous_stage.outputs.operations_container_registry_name
-# }
+output "ops_container_registry_name" {
+  value = data.terraform_remote_state.previous_stage.outputs.operations_container_registry_name
+}
 
 output "environment" {
   value = var.deployment_namespace
