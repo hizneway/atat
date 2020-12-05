1b04e1fc5838a2a59206ee6ea65c0e40b979909e
Jeff Deville <jeff@promptworks.com>
2020-12-05 16:28:03 -0500

    remove an interpoloation

--- terraform/providers/application_env/vpc.tf
+++ terraform/providers/application_env/vpc.tf
@@ -19,7 +19,7 @@ resource "azurerm_virtual_network" "vpc" {
   name                = "${var.name}-network-${var.deployment_namespace}"
   location            = azurerm_resource_group.vpc.location
   resource_group_name = azurerm_resource_group.vpc.name
-  address_space       = ["${var.virtual_network}"] # TODO(jesse) Can this be wired up dynamically?
+  address_space       = [var.virtual_network] # TODO(jesse) Can this be wired up dynamically?
   dns_servers         = []
 
   tags = {
