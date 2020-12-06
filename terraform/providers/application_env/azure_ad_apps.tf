a3957db8025e4c9bf7a7cca12f5e78cba55983cc
mikepw <mike@opsulent.com>
2020-12-05 22:09:04 -0500

    specify environment for service principles created

--- terraform/providers/application_env/azure_ad_apps.tf
+++ terraform/providers/application_env/azure_ad_apps.tf
@@ -1,24 +1,24 @@
 module "tenant_keyvault_app" {
-  source = "../../modules/azure_ad"
-  name   = "tenant-keyvault-${local.environment}"
+  source      = "../../modules/azure_ad"
+  name        = "tenant-keyvault-${local.environment}"
   environment = local.environment
 }
 
 
 module "aks_sp" {
-  source = "../../modules/azure_ad"
-  name   = "aks-service-principal-${local.environment}"
+  source      = "../../modules/azure_ad"
+  name        = "aks-service-principal-${local.environment}"
   environment = local.environment
 }
 
 module "bastion_sp" {
-  source = "../../modules/azure_ad"
-  name   = "bastion-service-principal-${local.environment}"
+  source      = "../../modules/azure_ad"
+  name        = "bastion-service-principal-${local.environment}"
   environment = local.environment
 }
 
 module "ops_keyvault_app" {
-  source = "../../modules/azure_ad"
-  name   = "ops-keyvault-sp-${local.environment}"
+  source      = "../../modules/azure_ad"
+  name        = "ops-keyvault-sp-${local.environment}"
   environment = local.environment
 }
