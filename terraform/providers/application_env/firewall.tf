9705c2739a622d66b6b3fcbd89ccab9139062409
Jeff Deville <jeff@promptworks.com>
2020-12-07 17:02:06 -0500

    fix route name collision

--- terraform/providers/application_env/firewall.tf
+++ terraform/providers/application_env/firewall.tf
@@ -41,7 +41,7 @@ resource "azurerm_route" "firewall_to_internet" {
 # }
 
 resource "azurerm_route" "fw_route_egress" {
-   name                   = "default"
+   name                   = "vnetlocal"
    resource_group_name    = azurerm_resource_group.vpc.name
    route_table_name       = azurerm_route_table.azure_firewall.name
    address_prefix         = "10.1.0.0/16"
