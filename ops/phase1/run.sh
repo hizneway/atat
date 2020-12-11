643cb0e5569a9b72827bdcc7041ff34de548c608
Jeff Deville <jeff@promptworks.com>
2020-12-11 13:48:47 -0500

    Guessing which log analytics workspace id they want

--- ops/phase1/run.sh
+++ ops/phase1/run.sh
@@ -76,7 +76,7 @@ az container create \
   --cpu 4 \
   --secure-environment-variables "OPS_RESOURCE_GROUP=$TF_VAR_resource_group_name" "OPS_STORAGE_ACCOUNT=$TF_VAR_storage_account_name" "SUBSCRIPTION_ID=$TF_VAR_operator_subscription_id" "SP_CLIENT_ID=$TF_VAR_operator_client_id" "SP_CLIENT_SECRET=$TF_VAR_operator_client_secret" "TENANT_ID=$TF_VAR_operator_tenant_id" "OPS_REGISTRY=$REGISTRY_NAME" "NAMESPACE=$1" "LOGGING_WORKSPACE_ID=$LOGGING_WORKSPACE_ID" "LOGGING_WORKSPACE_RESOURCE_ID=$LOGGING_WORKSPACE_RESOURCE_ID" \
   --command-line "tail -f /dev/null" \
-  --log-analytics-workspace $LOGGING_WORKSPACE \
+  --log-analytics-workspace $LOGGING_WORKSPACE_ID \
   --restart-policy Never
 
 echo "==============REMEMBER==============="
